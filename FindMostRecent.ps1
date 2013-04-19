1084be7b4193b7c3b41dacc3f21fe247c53d2861
owenblacker <owen.blacker@iris-worldwide.com>
2013-04-19 16:25:00 +0100

    PowerShell script to find the most-recent file in a directory tree

---
+++ FindMostRecent.ps1
@@ -0,0 +1,50 @@
+Function GetLatestFileInDir($dir)
+{
+	$mostRecent = $null
+	
+	foreach ($file in Get-ChildItem $dir)
+	{
+		if (Test-Path $file.FullName -PathType Container)
+		{
+			# Write-Host "Recursing into $($file.FullName)"
+			$recursed = GetLatestFileInDir(Get-Item $file.FullName)
+			if ($mostRecent -eq $null -or $recursed.LastWriteTimeUTC -gt $mostRecent.LastWriteTimeUTC)
+			{
+                # Write-Host "New latest file $($recursed.FullName) ($($recursed.LastWriteTimeUTC.ToString('f')))"
+				$mostRecent = $recursed
+			}
+		}
+		else
+		{
+			if ($mostRecent -eq $null -or $file.LastWriteTimeUTC -gt $mostRecent.LastWriteTimeUTC)
+			{
+                # Write-Host "New latest file $($file.FullName) ($($recursed.LastWriteTimeUTC.ToString('f')))"
+				$mostRecent = $file
+			}
+		}
+	}
+	
+	return Get-Item $mostRecent.FullName
+}
+
+Function Main()
+{
+	$startDir = $pwd
+	if ($args.Count -gt 0)
+	{
+		$startDir = Get-Item $args
+	}
+	
+	$latest = GetLatestFileInDir($startDir)
+
+	if ($latest -ne $null)
+	{
+		Write-Host "Most recent file is $($latest.FullName), updated $($latest.LastWriteTimeUTC.ToString('f'))"
+	}
+	else
+	{
+		Write-Host "Error: No file found"
+	}
+}
+
+Main
